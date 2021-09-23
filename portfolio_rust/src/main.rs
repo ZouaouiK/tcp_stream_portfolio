b2e2c59da911ef5c17dd06fcba21399255929fc1
ZouaouiK <73880704+ZouaouiK@users.noreply.github.com>
2021-09-23 13:25:11 +0100

    pack portfolio

--- portfolio_rust/src/main.rs
+++ portfolio_rust/src/main.rs
@@ -4,6 +4,7 @@ use std::net::TcpStream;
 use solana_program::pubkey::Pubkey;
 use byteorder::{ByteOrder, LittleEndian};
 
+use rand::Rng;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -11,9 +12,10 @@ fn main() {
  for stream in listener.incoming() {
      let stream = stream.unwrap();
 
-     unpack(stream);
+    // unpack(stream);
+     pack(stream);
  } 
- 
+
 }
 
 
@@ -90,6 +92,90 @@ fn unpack(mut stream: TcpStream) {
     stream.flush().unwrap();
 
 }
+fn pack(mut stream: TcpStream) {
+    let mut rng = rand::thread_rng();
+    let nb_asset: u8 = rng.gen::<u8>() % 10 + 1;
+    let asset_length = 66u8; 
+    let len_data=nb_asset as usize * asset_length as usize;
+  //  let mut buffer:Vec<u8> = Vec::with_capacity(147+(len_data as usize));
+  let mut buffer = [0; 8192];
+    //portfolioAddress
+    let  buffer_portfolio = [0; 32];
+    let  portfolio_address_key: Pubkey = Pubkey::new(&buffer_portfolio);//pubkey
+    println!("portfolio_address {}", portfolio_address_key); 
+
+    //creatorPortfolio
+    let  buffer_creator = [1; 32];
+    let  creator_address_key: Pubkey = Pubkey::new(&buffer_creator);//pubkey
+    println!(" creator_address {}", creator_address_key); 
+
+    //owner
+    let  buffer_owner = [2; 32];
+    let  owner_address_key: Pubkey = Pubkey::new(& buffer_owner);//pubkey
+    println!(" creator_address {}", owner_address_key); 
+
+    //metadataUrl
+    let  metadata_url=b"karima zouaoui and jawaher korbosli best friend";//47
+
+    //metdataHash
+
+    let  metadata_hash=999u16;
+    let x=metadata_hash.to_le_bytes();
+    let initialize=1u8;
+    let asset_len=2u8;
+    println!(" x {:?}", x); 
+   // println!("buffer before clone   {:?}",&buffer[0..]);
+    buffer[0..32].clone_from_slice(&buffer_portfolio);
+    buffer[32..64].clone_from_slice(&buffer_creator);
+    buffer[64..96].clone_from_slice(&buffer_owner);
+    buffer[96..143].clone_from_slice(metadata_url);
+    buffer[143..145].clone_from_slice(&x);
+    buffer[145]=initialize;
+    buffer[146]=nb_asset;
+    println!("buffer write  {:?}",&buffer[0..147]);
+   
+    //let mut asset_data:Vec<u8> = Vec::with_capacity(len_data as usize);
+   
+    let mut i = 0 as usize;
+    println!("i before while ={}",i); 
+    let offset = 0usize;
+   const asset_length2:usize=66;
+    const nb_asset2:usize=5;
+    const kk:usize=(nb_asset2* asset_length2) as usize;
+    println!("kkk {}",kk);
+    let mut asset_data=[0;660];
+    while i < nb_asset as usize  {
+        println!("i after while ={}",i); 
+        let start = offset + i * (asset_length as usize);
+        println!("i ={}",i); 
+        let amount=5u8;
+        let period=9u8;
+        let  address_asset =[3; 32];
+        let  asset_to_sold_into_asset = [4; 32];
+        asset_data[start..start+32].clone_from_slice(&address_asset);
+        println!("amount {}",amount);
+        asset_data[start+32]=amount;
+        asset_data[start+33..start+65].clone_from_slice(&asset_to_sold_into_asset);
+       // println!("start + 66 ={}",start + 66 );
+        asset_data[start+65]=period;
+         i = i+1;
+
+        
+    }
+
+   
+   buffer[147..807].clone_from_slice(&asset_data);
+    println!("buffer write  {:?}",  &buffer[0..807]);
+   // println!("asset data write  {:?}",&asset_data[0..kk]);
+   let mut buffer1:Vec<u8> = Vec::with_capacity(147+len_data);
+   buffer1.resize(147+len_data, 0);
+   buffer1.copy_from_slice(&buffer[0..147+len_data]);
+   println!("buffer1  {:?}",  &buffer1[0..]);
+    let response = "🍎 Narjassi \r\n\r\n";
+   
+    stream.write(response.as_bytes()).unwrap(); 
+    stream.flush().unwrap();
+} 
 
 
 
